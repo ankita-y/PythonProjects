d30c89b97e438403018324c3d6a083739d73add4
Anks <62417515+ankita-y@users.noreply.github.com>
2020-12-08 23:10:42 +0530

    Removed Commentedcode

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -10,15 +10,7 @@ def speech(text):
         os.system("text.wav")
     except Exception as e:
         print("Exception: ",e)
-        """gTTS(text=text,lang="en").write_to_fp(file)
-        file.seek(0)
-        pygame.mixer.init()
-        pygame.mixer.music.load(file)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy():
-            continue
-"""
-
+    
 
 if __name__ == '__main__':
     print("What should I say")
