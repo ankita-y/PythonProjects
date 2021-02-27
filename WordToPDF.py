45cc325a6520a3584e4169d703144fa21c05134c
Anks <62417515+ankita-y@users.noreply.github.com>
2021-02-28 01:28:28 +0530

    Word into PDF

---
+++ WordToPDF.py
@@ -0,0 +1,5 @@
+from docx2pdf import convert
+
+#Covert the word file into pdf
+convert(r"D:\Python_Projects\PythonProjects\Resume_Ankita_Yadav.docx",
+        r"D:\Python_Projects\PythonProjects\Resume_Ankita_Yadav.pdf")
